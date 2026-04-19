5a6d4ac72deae31f51f064873983feca6c3f3972
Blackspo0n <kmario-kellner@web.de>
2026-04-19 15:32:46 +0200

    logging

--- root/entry.sh
+++ root/entry.sh
@@ -9,7 +9,7 @@ echo "nobodyPassword\nnobodyPassword" | smbpasswd smbusr
 
 sleep 2
 echo "[Entry] Starting exec samba"
-exec smbd --log-stdout &
+exec smbd -d 10 --debug-stdout &
 
 
 echo "[Entry] Starting exec nmbd"
