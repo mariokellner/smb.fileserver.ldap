300534f30ea83c11bb128b3b38458c42e1534d18
Blackspo0n <kmario-kellner@web.de>
2026-05-11 15:19:36 +0200

    Bugfix ...

--- root/makeHome.sh
+++ root/makeHome.sh
@@ -6,7 +6,7 @@ USER_INFO=$(getent passwd "$2") || exit 1
 UID=$(echo "$USER_INFO" | cut -d: -f3)
 GID=$(echo "$USER_INFO" | cut -d: -f4)
 
-if [ ! -d "$1" ] && mkdir -p "$1"
+[ ! -d "$1" ] && mkdir -p "$1"
 
 chown "$UID:$GID" "$1"
 chmod 750 "$1"
